4620891f532ac82987cccb858803d3a6afe2dc16
Saemon Zixel <saemonzixel@gmail.com>
2020-02-23 23:18:09 +0400

    Refactored all (except parsing source). Recursion changed to infinity loop. Add call and access native JS objects. Default start scope now "window".

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # Little Lisp
 
-A mini Lisp interpreter in JavaScript.  Supports lists (obvs), function invocation, lambdas, lets, if statements, numbers, strings and the library functions `first`, `rest` and `print`.
+A mini Lisp interpreter in JavaScript. Supports lists (obvs), function invocation, lambdas, lets, if statements, numbers, strings. Use window as default scope. Can call a JS functions and read object fields, call object methods.
 
 * Original author: Mary Rose Cook <mary@maryrosecook.com>
-* Thanks to Martin Tornwall for the implementations of let and if.
 
 ## Examples
 
@@ -12,17 +11,31 @@ A mini Lisp interpreter in JavaScript.  Supports lists (obvs), function invocati
 ```
 
 ```lisp
-(first (1 2 3))
+(
+	(let x "ok!") 
+	(window.alert x)
+)
 ```
 
 ```lisp
-((lambda (x) (rest x)) ("a" "b" "c"))
+(
+	(let x "ok!") 
+	(window.alert (x.substring 0 2))
+)
 ```
 
 ```lisp
-((let x "ok!") (print x))
+((let x 1) (if (x) (alert "x == 1") (alert "x != 1")))
 ```
 
 ```lisp
-((let x 1) (if (x) (print "x == 1") (print "x != 1")))
+((lambda (x) (window.alert x)) ("a" "b" "c"))
 ```
+
+```lisp
+(
+	(let f1 (lambda (x y) (alert x)))
+	(f1 "f1 - ok!")
+)
+```
+
--- littlelisp.js
+++ littlelisp.js
@@ -1,94 +1,316 @@
 ;(function(exports) {
-var library = {
-	first: function(x) {
-		return x[0];
-	},
 
-	rest: function(x) {
-		return x.slice(1);
-	},
-
-	print: function(x) {
-		console.log(x);
-		return x;
-	}
-};
-
-var Context = function(scope, parent) {
+var Context = function(scope, parent, input, index) {
 	this.scope = scope;
 	this.parent = parent;
+	this.input = input;
+	this.indx = index || -1;
+	this.result = [];
+	this.thiz = (parent || {}).thiz;
 
-	this.get = function(identifier) {
+	this.get = function(identifier, set_ctx_thiz) {
+		
+		// obj.field ?
+		if (identifier.indexOf(".") > -1) {
+			var prefix = identifier.substring(0, identifier.indexOf("."));
+			var obj = undefined;
+			
+			if (prefix in this.scope)
+				obj = this.scope[prefix];
+			else if (this.parent !== undefined)
+				obj = this.parent.get(prefix);
+			
+			if(set_ctx_thiz)
+				this.thiz = obj;
+			
+			if(obj)
+				return obj[identifier.substring(identifier.indexOf(".")+1)];
+			else
+				return undefined;
+		}
+		
 		if (identifier in this.scope)
 			return this.scope[identifier];
 		
 		if (this.parent !== undefined)
 			return this.parent.get(identifier);
 	};
+	
+	this.set = function(identifier, value) {
+		
+		// obj.field = value ?
+		if (identifier.indexOf(".") > -1) {
+			var prefix = identifier.substring(0, identifier.indexOf("."));
+			var obj = undefined;
+			
+			if (prefix in this.scope)
+				obj = this.scope[prefix];
+			else if (this.parent !== undefined)
+				obj = this.parent.get(prefix);
+			
+			// если не существует объект, то создадим новый
+			if (obj === undefined || obj === null)
+				throw "Error! "+prefix+" is "+obj+"!";
+			
+			obj[identifier.substring(identifier.indexOf(".")+1)] = value;
+			return value;
+		}
+		
+		this.scope[identifier] = value;
+		return value;
+	}
 };
 
-var special = {
-	let: function(input, context) {
-		context.scope[input[1]] = interpret(input[2], context);
-		return context.scope[input[1]];
-	},
+var interpret = function(input, scope) {
+	
+	// lisp
+	if(input instanceof Array) {
+		var ctx = new Context(scope || window, undefined, input, -1);
+// 		return interp_loop(ctx);
+		
+		while(ctx.indx < ctx.input.length) {
+			ctx = interp_step(ctx);
+		}
+	
+		return ctx.result;
+	}
+	
+	// var
+	else if (input.type === "identifier")
+		return (scope || window)[ input[0].value ];
+			
+	// string/number
+	else if (input.type === "number" || input.type === "string")
+		return input.value;
+	
+	// ???
+	else
+		throw "Unrecognized - "+JSON.stringify(input);
+}
 
-	lambda: function(input, context) {
-		return function() {
+var interp_step = function(ctx) {
+	
+	/* обработаем тек.элемент */
+	if (ctx.indx > -1) {
+
+		if (ctx.input[ctx.indx] instanceof Array) {
+			ctx = new Context(ctx.scope, ctx, ctx.input[ctx.indx], -1);
+		}
+		
+		// var
+		else if (ctx.input[ctx.indx].type === "identifier") {
+			ctx.result[ctx.indx] = ctx.get(ctx.input[ctx.indx].value, ctx.indx == 0);
+		}
+			
+		// string/number
+		else if (ctx.input[ctx.indx].type === "number" || ctx.input[ctx.indx].type === "string") {
+			ctx.result[ctx.indx] = ctx.input[ctx.indx].value;
+		}
+			
+		// ???
+		else
+			throw new Exception("Unrecognized - "+ctx.input[ctx.indx]);
+	}
+	
+	/* решим куда сдвигать курсор */
+	while(1)
+	
+	// (let x ...)
+	if (ctx.input[0].value == "let") {
+		
+		// let
+		if (ctx.indx == -1) {
+			ctx.indx = 2;
+			return ctx;
+		}
+		
+		// let-value
+		if (ctx.indx == 2) {
+			ctx.scope[ctx.input[1].value] = ctx.result[2];
+			ctx.result = ctx.result[2];
+			
+			// parent
+			if (ctx.parent) {
+				ctx.parent.result[ctx.parent.indx] = ctx.result;
+				ctx = ctx.parent;
+				continue;
+			}
+			
+			// no-parent
+			if (ctx.parent === undefined) {
+				ctx.indx = ctx.input.length;
+				return ctx;
+			}
+		}
+		
+		debugger;
+	}
+		
+	// (if ... (...) (...))
+	else if (ctx.input[0].value == "if") {
+		
+		// if
+		if (ctx.indx == -1) {
+			ctx.indx = 1;
+			return ctx;
+		}
+		
+		// if-test
+		if (ctx.indx == 1) {
+			
+			// -> true
+			if (!!ctx.result[1]) { 
+				ctx.indx = 2; 
+				return ctx; 
+			}
+			
+			// -> false
+			if (ctx.input[3] !== undefined) { 
+				ctx.indx = 3; 
+				return ctx; 
+			}
+			
+			ctx.result = undefined;
+			
+			// parent
+			if (ctx.parent) {
+				ctx.parent.result[ctx.parent.indx] = ctx.result;
+				ctx = ctx.parent;
+				continue;
+			}
+			
+			// no-parent
+			if (ctx.parent === undefined) {
+				ctx.indx = ctx.input.length;
+				return ctx;
+			}
+		}
+		
+		// if-true,if-false
+		if (ctx.indx == 2 || ctx.indx == 3) {
+			ctx.result = ctx.result[ctx.indx];
+			ctx.indx = ctx.input.length;
+			
+			// parent
+			if (ctx.parent) {
+				ctx.parent.result[ctx.parent.indx] = ctx.result;
+				ctx = ctx.parent;
+				continue;
+			}
+			
+			// no-parent
+			if (ctx.parent === undefined) {
+				return ctx;
+			}
+		}
+		
+		debugger;
+	}
+	
+	// (lambda (x y) (...))
+	else if (ctx.input[0].value == "lambda") {
+		var input = ctx.input;
+		ctx.result = function() {
 			var lambdaScope = {};
 			var lambdaArgNames = input[1];
 			for(var i = 0; i < lambdaArgNames.length; i++)
 				lambdaScope[lambdaArgNames[i].value] = arguments[i] || undefined;
-
-			return interpret(input[2], new Context(lambdaScope, context));
+			
+			return new Context(lambdaScope, undefined, input[2]);
 		};
-	},
-
-	if: function(input, context) {
-		return interpret(input[1], context) ?
-			interpret(input[2], context) :
-			interpret(input[3], context);
+		
+		ctx.indx = ctx.input.length;
+		
+		// parent
+		if (ctx.parent) {
+			ctx.parent.result[ctx.parent.indx] = ctx.result;
+			ctx = ctx.parent;
+			continue;
+		}
+		
+		// no-parent
+		if (ctx.parent === undefined) {
+			ctx.indx = ctx.input.length;
+			return ctx;
+		}
+		
+		debugger;
 	}
-};
+	
+	// (<func> ...)
+	else if (ctx.result[0] instanceof Function) {
+		
+		// вычеслим след. аргумент
+		if (ctx.indx+1 < ctx.input.length) {
+			ctx.indx++;
+			return ctx;
+		}
+		
+		// вызов завершён -> возвращаем результат наверх
+		else if (ctx.indx == ctx.input.length) {
+			ctx.result = ctx.result[ctx.indx];
+			
+			// parent
+			if (ctx.parent) {
+				ctx.parent.result[ctx.parent.indx] = ctx.result;
+				ctx = ctx.parent;
+				continue;
+			}
+			
+			// no-parent
+			else {
+				return ctx;
+			}
+		}
+		
+		// аргументы готовы -> делаем вызов функции
+		else {
+			var call_result = ctx.result[0].apply(ctx.thiz, ctx.result.slice(1));
+			
+			ctx.indx = ctx.input.length; // отметим, что вызов был сделан уже
+			
+			// если вернули Context то это наша lambda, def...
+			if(call_result instanceof Context) {
+				call_result.parent = ctx; // управление должно будет вернуться к нам
+				return call_result;
+			}
+			
+			// иначе простая функция JS была вызвана
+			else {
+				ctx.result[ctx.indx] = call_result;
+				continue;
+			}
+		}
+	}
+	
+	// (....)
+	else {
+		
+		// двигаемся в право ->
+		ctx.indx++;
+		if (ctx.indx < ctx.input.length)
+			return ctx;
+		
+		// иначе наверх ^|
+		else if (ctx.parent) {
+			ctx.parent.result[ctx.parent.indx] = ctx.result;
+			ctx = ctx.parent;
+			continue;
+		}
+		
+		// выше некуда - finish
+		else {
+			ctx.indx = ctx.input.length;
+			return ctx;
+		}
+	}
 
-var interpretList = function(input, context) {
-	
-	// оператор языка? - передадим в обработчик управление
-	if (input.length > 0 && input[0].value in special)
-		return special[input[0].value](input, context);
-	
-	// отработаем все элементы списка
-	var list = input.map(function(x) { 
-		return interpret(x, context); 
-	});
-	
-	// функция? - вернём результат функции
-	if (list[0] instanceof Function)
-		return list[0].apply(undefined, list.slice(1));
-
-	// иначе просто список возвращяем
-	return list;
-};
-
-var interpret = function(input, context) {
-	// первый запуск
-	if (context === undefined)
-		return interpret(input, new Context(library));
-	
-	if (input instanceof Array)
-		return interpretList(input, context);
-	
-	if (input.type === "identifier")
-		return context.get(input.value);
-	
-	if (input.type === "number" || input.type === "string")
-		return input.value;
-};
+}
 
 // токены -> дерево списков
-var parse_lists = function(input, list) {
+var analize = function(input, list) {
 	if (list === undefined)
-		return parse_lists(input, []);
+		return analize(input, []);
 	
 	// берём токен на распознание
 	var token = input.shift();
@@ -97,8 +319,8 @@ var parse_lists = function(input, list) {
 		return list.pop();
 	
 	if (token === "(") {
-		list.push(parse_lists(input, []));
-		return parse_lists(input, list);
+		list.push(analize(input, []));
+		return analize(input, list);
 	} 
 	
 	if (token === ")")
@@ -113,9 +335,10 @@ var parse_lists = function(input, list) {
 	else 
 		list.push({ type:'identifier', value: token });
 	
-	return parse_lists(input, list);
+	return analize(input, list);
 };
 
+// строка -> токены
 var tokenize = function(input) {
 	return input.split('"')
 			.map(function(x, i) {
@@ -134,8 +357,9 @@ var tokenize = function(input) {
 			});
 };
 
+// tokenize+analize
 var parse = function(input) {
-	return parse_lists(tokenize(input));
+	return analize(tokenize(input));
 };
 
 exports.littleLisp = {
